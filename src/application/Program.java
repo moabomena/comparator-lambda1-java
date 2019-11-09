616bf9e4f499d075f3a9dc15dc3e11056c6d8592
Moab Omena <1moabomena@gmail.com>
2019-11-09 18:52:24 -0300

    Anonymous class

---
+++ src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		Comparator<Product> comp = new Comparator<Product>() {
+			
+			@Override
+			public int compare(Product p1, Product p2) {
+				return  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+
+		list.sort(comp);
+		
+		for (Product pro : list) {
+			System.out.println(pro);
+		}
+
+	}
+
+}
